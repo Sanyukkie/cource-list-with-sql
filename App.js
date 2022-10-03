e90d53caa0aec1360c094f7e81714a7318f1c0ca
Aleksander Truss <aleksander.truss@nordhealth.com>
2022-10-03 17:10:33 +0300

    Exercise complete

--- App.js
+++ App.js
@@ -1,11 +1,63 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, View, Button, FlatList } from 'react-native';
+import * as SQLite from'expo-sqlite';
+import React, { useState, useEffect } from 'react';
 
 export default function App() {
+  
+  const [credit, setCredit] = useState('');
+  const [title, setTitle] = useState('');
+  const [courses, setCourses] = useState([]);
+
+  const db = SQLite.openDatabase('coursedb.db');
+
+  useEffect(() => {  
+    db.transaction(tx => {    
+      tx.executeSql('create table if not exists course (id integer primary key    not null, credits int, title text);');  
+    }, null, updateList);}, []);
+
+  const saveItem = () => {  
+    db.transaction(tx => {    
+      tx.executeSql('insert into course (credits, title) values (?, ?);',  
+      [parseInt(credit), title]);    
+    }, null, updateList)}
+
+  const updateList = () => {  
+    db.transaction(tx => {    
+      tx.executeSql('select * from course;', [], (_, { rows }) =>      
+      setCourses(rows._array)    
+      );   
+    }, null, null);
+  }
+
+  const deleteItem = (id) => {  
+    db.transaction(
+      tx => tx.executeSql('delete from course where id = ?;', [id]), null, updateList) 
+    }
+  
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <TextInput  
+      style={styles.input}
+      placeholder='Title'  
+      onChangeText={title => setTitle(title)}  
+      value={title}/> 
+      <TextInput 
+      placeholder='Credits'
+      keyboardType='numeric'
+      onChangeText={credit => setCredit(credit)}  
+      value={credit}/>
+      <Button onPress={saveItem} title="Save" />
+      <FlatList 
+      style={{marginLeft : "5%"}}  
+      keyExtractor={item => item.id.toString()}   
+      renderItem={({item}) =>
+        <View style={styles.list}>
+          <Text>{item.title},{item.credits} </Text>
+          <Text style={{color: '#0000ff'}} onPress={() => deleteItem(item.id)}>done</Text>
+        </View>}        
+        data={courses} 
+      />
     </View>
   );
 }
@@ -15,6 +67,13 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
+  },
+  input: {
+    marginTop: 50,
+    marginBottom: 5,
+    width: 200,
+    borderColor: 'gray',
+    borderWidth: 1 
   },
 });
